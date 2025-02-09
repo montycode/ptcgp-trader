42d984486c4ce9aa5f949fcda627f067f08846b2
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2025-02-08 21:59:32 -0800

    qstash

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,10 +2,33 @@ import React, { ReactNode } from "react";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
+import { after } from "node:test";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
+
   if (!session) redirect("/login");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl ">
